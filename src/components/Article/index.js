147aaebe183f664dfb82eadb1ac7a35593d74e5f
hugomllo <hugomattusdd@gmail.com>
2022-09-08 21:56:49 -0300

    criação dos componentes

---
+++ src/components/Article/index.js
@@ -0,0 +1,13 @@
+import Post from '../Post'
+
+
+const Article = () =>{
+    return(
+        <>
+        <h1>Article</h1>
+        <Post></Post>
+        </>
+    )
+}
+
+export default Article;
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+    return(
+        <>
+        <h1>Header.js</h1>
+        </>
+    )
+}
+
+
+
+export default Header;
